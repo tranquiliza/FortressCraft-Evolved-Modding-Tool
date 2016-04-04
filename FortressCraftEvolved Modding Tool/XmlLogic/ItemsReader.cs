8c55e7412619b5d829f6c708fa5106268584049c
tranquiliza <daniel.olsen.server@gmail.com>
2016-04-04 09:14:43 +0200

    Initial Commit

---
+++ FortressCraftEvolved Modding Tool/XmlLogic/ItemsReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FortressCraftEvolved_Modding_Tool.XmlLogic
+{
+    class ItemsReader
+    {
+    }
+}
